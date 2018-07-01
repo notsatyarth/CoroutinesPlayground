011afe29970ec0938dcaed469037808ab24fd573
satyarth.sampath <satyarth.sampath@go-jek.com>
2018-07-01 16:28:04 +0530

    Improve exception variety

--- app/src/test/java/an/droids/coroutines/errorhandling/ExceptionHandlersTest.kt
+++ app/src/test/java/an/droids/coroutines/errorhandling/ExceptionHandlersTest.kt
@@ -1,20 +1,25 @@
 package an.droids.coroutines.errorhandling
 
 import an.droids.coroutines.log
-import kotlinx.coroutines.experimental.joinAll
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
 
 class ExceptionHandlersTest {
     @Test
-    fun `test exception handling on coroutines`() = runBlocking {
-        val runWithoutException = launch(fallibleThreadPool) {
-            log("Without Exception")
+    fun `test uncaught exception handling on coroutines`() = runBlocking {
+        val runWithoutException = List(100) { idx ->
+            launch(fallibleThreadPool) {
+                when {
+                    idx % 2 == 0 -> 1.div(0) //divide by zero
+                    idx % 3 == 0 -> listOf(1)[2] // Index out of bounds
+                    idx % 4 == 0 -> emptyMap<String, String>()["InvalidKey"]!!.contains("a") //NPE
+                    idx % 5 == 0 -> throw Throwable()
+                    else -> log("Without Exception")
+                }
+            }
         }
-        val runWithException = launch(fallibleThreadPool) {
-            throw IllegalAccessException("Throws an error")
-        }
-        joinAll(runWithException, runWithoutException)
+        runWithoutException.forEach { it.join() }
     }
+
 }
